77c74fc770379da87ffa293c31b31fdea4667d2a
Caleb Madrigal <caleb.madrigal@gmail.com>
2018-03-24 13:14:29 -0500

    Bit more documentation.

--- trigger_plugin_class.py
+++ trigger_plugin_class.py
@@ -5,7 +5,8 @@ class Trigger:
     def __init__(self):
         self.devices_seen = set()
 
-    def __call__(self, dev_id=None, num_bytes=None, power=None, **kwargs):
+    def __call__(self, dev_id=None, **kwargs):
+        """Note that we can specify any subset of arguments we care about... in this case, just dev_id."""
         self.devices_seen |= {dev_id}
         print('devices_seen: {}'.format(self.devices_seen))
 
--- trigger_plugin_func.py
+++ trigger_plugin_func.py
@@ -2,6 +2,7 @@ __apiversion__ = 1
 
 
 def trigger(dev_id=None, num_bytes=None, power=None, **kwargs):
+    """Note that we can specify any subset of kwargs we are interested in."""
     if num_bytes:
         print('Threshold reached for {} - {} bytes'.format(dev_id, num_bytes))
     else:
